059ac7e5981b341b040d54f2b59160e3187ba374
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-06 06:14:57 +0100

    Added my hero section

---
+++ components/hero/index.js
@@ -0,0 +1,25 @@
+
+
+export default function Hero() {
+  return (
+    <section className="lg:2/6 text-left my-28">
+      <div className="text-6xl font-semibold text-gray-900 leading-none">
+        Purchase your property now
+      </div>
+      <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">
+        Discover Your Dream Home: Where Every Door Opens to Possibility.
+        Ready to Roam? Let's Find Your Next Dream Dwelling Together.
+      </div>
+      <div className="mt-5 sm:mt-8 flex lg:justify-start">
+        <div className="rounded-md shadow">
+          <a
+            href="#"
+            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 md:py-4 md:text-lg md:px-10"
+          >
+            Let's go
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
+import Hero from "@/components/hero"
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Footer />
+      {/* <Footer /> */}
+      <Hero />
     </>
   )
 }
