aee26f15a4c872b5c86f85ece706ff45499411e9
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-06 06:42:01 +0100

    Added my info, breadcrumb  and wallet component

---
+++ components/infowallet/index.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+export default function InfoWallet() {
+  const [ethPrice, setEthPrice] = useState(0);
+  const [coursePrice, setCoursePrice] = useState(0.004769);
+  const [conversionRate, setConversionRate] = useState(0);
+
+  useEffect(() => {
+    // Fetch the ETH price from an API
+    fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+      .then(response => response.json())
+      .then(data => {
+        setEthPrice(data.ethereum.usd);
+      })
+      .catch(error => {
+        console.error('Error fetching ETH price:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    setConversionRate(coursePrice * ethPrice);
+  }, [ethPrice, coursePrice]);
+
+    return (
+            <div className="grid grid-cols-4 mb-5">
+                <div className="flex flex-1 items-stretch text-center">
+                    <div className="p-10 border drop-shadow rounded-md">
+                        <div>
+                            <span className="text-2xl font-bold">ETH = ${ethPrice.toFixed(2)}</span>
+                        </div>
+                        <p className="text-xl text-gray-500">Current ETH Price</p>
+                    </div>
+                </div>
+                <div className="flex flex-1 items-stretch text-center">
+                    <div className="p-10 border drop-shadow rounded-md">
+                        <div>
+                            <span className="text-2xl font-bold">{coursePrice.toFixed(6)} ETH = ${conversionRate.toFixed(2)}</span>
+                        </div>
+                        <p className="text-xl text-gray-500">Property price</p>
+                    </div>
+                </div>
+            </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 import Hero from "@/components/hero"
+import InfoWallet from "@/components/infowallet"
+import Breadcrumbs from "@/components/breadcrumbs"
+import WalletAddress from "@/components/walletaddress"
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      {/* <Footer /> */}
       <Hero />
+      <Breadcrumbs />
+      <WalletAddress />
+      <InfoWallet />
+      <Footer />
     </>
   )
 }
