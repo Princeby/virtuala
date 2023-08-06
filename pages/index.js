2c000cb66ce7051a54eacf756412a4a7c25f5145
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-06 13:26:11 +0100

    added image and made a general style in tailwind

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,20 @@ import WalletAddress from "@/components/walletaddress"
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Breadcrumbs />
-      <WalletAddress />
-      <InfoWallet />
-      <Footer />
+      <div className="bg-[url('/assets/background.jpeg')]">
+        <Navbar />
+        <div className="relative bg-white overflow-hidden">
+          <div className="relative max-w-7xl mx-auto px-4">
+            <div className="fit">
+              <Hero />
+              <Breadcrumbs />
+              <WalletAddress />
+              <InfoWallet />
+              <Footer />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
