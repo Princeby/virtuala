8110f416ba44196e33defb7fe201a6564fd22ca8
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-06 14:23:41 +0100

    added apartment images

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,23 @@ import Hero from "@/components/hero"
 import InfoWallet from "@/components/infowallet"
 import Breadcrumbs from "@/components/breadcrumbs"
 import WalletAddress from "@/components/walletaddress"
+import OrderHistory from "@/components/orderhistory"
+import Items from "@/components/items"
 
 export default function Home() {
   return (
     <>
-      <div className="bg-[url('/assets/background.jpeg')]">
-        <Navbar />
-        <div className="relative bg-white overflow-hidden">
-          <div className="relative max-w-7xl mx-auto px-4">
-            <div className="fit">
-              <Hero />
-              <Breadcrumbs />
-              <WalletAddress />
-              <InfoWallet />
-              <Footer />
-            </div>
+      <Navbar />
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto px-4">
+          <div className="fit">
+            <Hero />
+            <Breadcrumbs />
+            <WalletAddress />
+            <InfoWallet />
+            <OrderHistory />
+            <Items />
+            <Footer />
           </div>
         </div>
       </div>
