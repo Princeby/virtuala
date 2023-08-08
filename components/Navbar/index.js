8f6cb26232b1ef9da5b8ecb6cbcc183c65a0b865
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-08 20:09:49 +0100

    changed the a tag to a link

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import Image from "next/image";
 
 const navigation = [
-  { name: "Home", href: "#" },
+  { name: "Home", href: "/" },
   { name: "Marketplace", href: "#" },
   { name: "About", href: "#" },
 ];
@@ -22,10 +24,12 @@ export default function Navbar() {
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">virtuala</span>
-                <img
+                <Image
                   className="h-32 w-auto"
-                  src="assets/logo/logo_transparent.png"
+                  src="/assets/logo/logo_transparent.png"
                   alt=""
+                  width="128"
+                  height="128"
                 />
               </a>
             </div>
@@ -41,13 +45,13 @@ export default function Navbar() {
             </div>
             <div className="hidden lg:flex lg:gap-x-12">
               {navigation.map((item) => (
-                <a
+                <Link
                   key={item.name}
                   href={item.href}
                   className="text-sm font-semibold leading-6 text-gray-900"
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -95,13 +99,13 @@ export default function Navbar() {
                 <div className="-my-6 divide-y divide-gray-500/10">
                   <div className="space-y-2 py-6">
                     {navigation.map((item) => (
-                      <a
+                      <Link
                         key={item.name}
                         href={item.href}
                         className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                       >
                         {item.name}
-                      </a>
+                      </Link>
                     ))}
                   </div>
                   <div className="py-6">
