852fbffed794d32305dea12e29ab2488c8e145a9
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-08 15:14:59 +0100

    added my data into the item components

--- components/items/index.js
+++ components/items/index.js
@@ -1,17 +1,58 @@
 
-export default function Items() {
+// export default function Items() {
+//     return (
+//       <section className="grid grid-cols-2 gap-4 mb-5">
+//         { Array.from({length: 4}).map((_, i) =>
+//           <div key={i} className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+//             <div className="md:flex">
+//               <div className="md:flex-shrink-0">
+//                 <img className="h-48 w-full object-cover md:w-48" src={`assets/images/apartment${i}.jpg`} alt="Apartment building" />
+//               </div>
+//               <div className="p-8">
+//                 <div className="uppercase tracking-wide text-sm text-orange-500 font-semibold">Case study</div>
+//                 <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding a property for you.</a>
+//                 <p className="mt-2 text-gray-500">Getting a new property is a lot of hard work. Here, we provide are five ideas you can use to find your property.</p>
+//               </div>
+//             </div>
+//           </div>
+//         )}
+//       </section>
+//     )
+//   }
+
+
+
+
+
+export default function Items({properties}) {
     return (
       <section className="grid grid-cols-2 gap-4 mb-5">
-        { Array.from({length: 4}).map((_, i) =>
-          <div key={i} className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        {  properties.map(property =>
+          <div
+            key={property.id}
+            className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
             <div className="md:flex">
               <div className="md:flex-shrink-0">
-                <img className="h-48 w-full object-cover md:w-48" src={`assets/images/apartment${i}.jpg`} alt="Apartment building" />
+                <img
+                  className="h-48 w-full object-cover md:w-48"
+                  src={property.coverImage}
+                  alt={property.title}
+                />
               </div>
               <div className="p-8">
-                <div className="uppercase tracking-wide text-sm text-orange-500 font-semibold">Case study</div>
-                <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding a property for you.</a>
-                <p className="mt-2 text-gray-500">Getting a new property is a lot of hard work. Here, we provide are five ideas you can use to find your property.</p>
+                <div
+                  className="uppercase tracking-wide text-sm text-orange-500 font-semibold">
+                  {property.type}
+                </div>
+                <a
+                  href="#"
+                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                  {property.title}
+                </a>
+                <p
+                  className="mt-2 text-gray-500">
+                  {property.description}
+                </p>
               </div>
             </div>
           </div>
