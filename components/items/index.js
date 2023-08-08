00b58293e8c4881bc1fe46c85c495c08b1b846ff
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-08 20:08:17 +0100

    made an alteration to my image property

--- components/items/index.js
+++ components/items/index.js
@@ -1,54 +1,32 @@
-
-// export default function Items() {
-//     return (
-//       <section className="grid grid-cols-2 gap-4 mb-5">
-//         { Array.from({length: 4}).map((_, i) =>
-//           <div key={i} className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-//             <div className="md:flex">
-//               <div className="md:flex-shrink-0">
-//                 <img className="h-48 w-full object-cover md:w-48" src={`assets/images/apartment${i}.jpg`} alt="Apartment building" />
-//               </div>
-//               <div className="p-8">
-//                 <div className="uppercase tracking-wide text-sm text-orange-500 font-semibold">Case study</div>
-//                 <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding a property for you.</a>
-//                 <p className="mt-2 text-gray-500">Getting a new property is a lot of hard work. Here, we provide are five ideas you can use to find your property.</p>
-//               </div>
-//             </div>
-//           </div>
-//         )}
-//       </section>
-//     )
-//   }
-
-
-
+import Image from "next/image"
+import Link from "next/link"
 
 
 export default function Items({properties}) {
     return (
-      <section className="grid grid-cols-2 gap-4 mb-5">
-        {  properties.map(property =>
+      <section className="grid md:grid-cols-2 gap-4 mb-5">
+        { properties.map(property =>
           <div
             key={property.id}
             className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-            <div className="md:flex">
-              <div className="md:flex-shrink-0">
-                <img
-                  className="h-48 w-full object-cover md:w-48"
+            <div className="h-full md:flex">
+              <div className=" md:flex-shrink-0">
+                <Image
+                  className="h-full w-full object-cover md:w-48"
                   src={property.coverImage}
                   alt={property.title}
+                  width="200"
+                  height="230"
                 />
-              </div>
+              </div> 
               <div className="p-8">
                 <div
                   className="uppercase tracking-wide text-sm text-orange-500 font-semibold">
                   {property.type}
                 </div>
-                <a
-                  href="#"
-                  className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
-                  {property.title}
-                </a>
+                <Link href={`/apartments/${property.slug}`} className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                    {property.title}
+                </Link>
                 <p
                   className="mt-2 text-gray-500">
                   {property.description}
