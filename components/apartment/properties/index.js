fe682726f927ae406fd9d340167404e1f847841c
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-09 08:24:54 +0100

    added a props value

--- components/apartment/properties/index.js
+++ components/apartment/properties/index.js
@@ -9,7 +9,9 @@ const properties = [
     "Respara",
   ]
   
-  export default function Property() {
+  export default function Property({status}) {
+
+    const statusValue = `px-2 inline-flex text-xs leading-5 font-semibold rounded-full`;
   
     return (
       <section className="max-w-5xl mx-auto">
@@ -44,12 +46,17 @@ const properties = [
                           </div>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap">
-                          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                            Unlocked
+                          <span className={ 
+                            status ?
+                            `bg-red-100 text-red-800 ${statusValue}` :
+                            `bg-green-100 text-green-800 ${statusValue}`
+                            }
+                            >
+                            { status ? "Unavailable" : "Available" }
                           </span>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          <a href="#" className="text-orange-600 hover:text-orange-900">Play</a>
+                          <a href="#" className="text-orange-600 hover:text-orange-900">{status ? "Purchase":""}</a>
                         </td>
                       </tr>
                     )}
