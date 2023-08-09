1cac13a8972dc91ba2fc84cc58292e1af4c2e0ad
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2023-08-09 08:20:47 +0100

    added some props to my slug page

--- pages/apartments/[slug].js
+++ pages/apartments/[slug].js
@@ -3,17 +3,49 @@ import Property from "@/components/apartment/properties"
 import Modal from "@/components/apartment/modal"
 import PropertyHero from "@/components/apartment/hero"
 import Layout from "@/components/layout"
+import { getAllProperty } from "@/content/fetcher"
 
 
 
-export default function Apartment() {
+export default function Apartment({property}) {
 
     return (
         <Layout>
-            <PropertyHero />
-            <BulletPoints />
+            <PropertyHero
+                title={property.title}
+                description={property.description}
+                image={property.coverImage}
+            />
+            <BulletPoints
+                points={property.features}    
+            />
             <Property />
             <Modal />
         </Layout>
     )
 }
+
+export function getStaticPaths() {
+    const { data } = getAllProperty()
+
+    return {
+        paths: data.map(c => ({
+            params: {
+                slug: c.slug
+            }
+        })),
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}) {
+    const { data } = getAllProperty()
+
+    const property = data.filter(c => c.slug === params.slug)[0]
+
+    return {
+      props: {
+        property
+      }
+    }
+  }
